7e8f14e7944edbad2e5f27c45887bfc3223883c6
Dharmik <dharmik6610@gmail.com>
2023-06-13 12:58:15 +0530

    initial commit

---
+++ Components/Tags/ImgComponent.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using HTMLToQPDF.Components;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace HTMLQuestPDF.Components.Tags
+{
+    internal class ImgComponent : BaseHTMLComponent
+    {
+        private readonly GetImgBySrc getImgBySrc;
+
+        public ImgComponent(HtmlNode node, HTMLComponentsArgs args) : base(node, args)
+        {
+            this.getImgBySrc = args.GetImgBySrc;
+        }
+
+        protected override void ComposeSingle(IContainer container)
+        {
+            var src = node.GetAttributeValue("src", "");
+            var img = getImgBySrc(src) ?? Placeholders.Image(200, 100);
+            container.Image(img, ImageScaling.FitArea);
+        }
+    }
+}
